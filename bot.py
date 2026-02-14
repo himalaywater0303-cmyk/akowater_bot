b44423038ed32d5b80a5dfea7a815eac0cf4e0c7
himalaywater0303-cmyk <himalaywater0303@gmail.com>
2026-02-14 23:42:42 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-8427218470:AAFNC81mmMl8d0op2xJzS7I8Vg5vOC8vPbo
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Assalomu alaykum! AkoWater botiga xush kelibsiz 💧")
+
+bot.polling(none_stop=True)
